e463cc3ab76853786c61346298704edb0ea06de3
Lale159 <lale159@gmail.com>
2015-03-16 14:28:46 -0700

    testing

---
+++ test.java
@@ -0,0 +1,12 @@
+package githubtest;
+
+public class newJavaFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		System.out.println("What is a?? = " + a);
+
+	}
+
+}
